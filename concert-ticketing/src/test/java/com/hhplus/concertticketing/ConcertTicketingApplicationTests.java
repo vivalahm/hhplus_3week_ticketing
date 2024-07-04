486259a72e31fab257c24ce2c6e0bea9333e65fd
HMChoi <48741014+vivalahm@users.noreply.github.com>
2024-07-05 00:16:57 +0900

    initial commit

---
+++ concert-ticketing/src/test/java/com/hhplus/concertticketing/ConcertTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.concertticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
